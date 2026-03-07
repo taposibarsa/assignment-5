52d5986360cbe040d70d65bfe82fdc2635c62d9b
taposi barsa <taposibarsa@gmail.com>
2026-03-08 00:14:59 +0600

    i am initializing git on vs code and connecting this with github

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById("signin").addEventListener("click", () => {
+  const userBtn = document.getElementById("username-btn").value;
+  const passBtn = document.getElementById("pass-btn").value;
+  console.log("userBtn");
+
+    if (userBtn === "admin" && passBtn === "admin123") {
+    alert("login success");
+    window.location.replace("/home.html");
+  } else {
+    alert("login failed");
+    return;
+  }
+})
